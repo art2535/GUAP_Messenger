c540ce47c4407b1da1da7322769878ddadeb677e
art2535 <artem010606@yandex.ru>
2025-10-04 22:55:00 +0300

    Созданы объекты передачи данных (DTOs)

---
+++ Messenger.API/DTOs/LoginRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Messenger.API.DTOs
+{
+    public class LoginRequest
+    {
+        [Required]
+        public string Login { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
---
+++ Messenger.API/DTOs/LogoutRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Messenger.API.DTOs
+{
+    public class LogoutRequest
+    {
+        [Required]
+        public string Login { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public string Token { get; set; }
+    }
+}
