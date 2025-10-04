faef3b16e8be7136ade387c95079ca7c14e1cd12
art2535 <artem010606@yandex.ru>
2025-10-04 22:56:32 +0300

    Обновлен файл Program.cs

--- Messenger.API/Program.cs
+++ Messenger.API/Program.cs
@@ -1,3 +1,4 @@
+using Messenger.API.Extensions;
 
 namespace Messenger.API
 {
@@ -7,25 +8,29 @@ namespace Messenger.API
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            /*if (builder.Environment.IsDevelopment())
+            {
+                JwtExtension.SetTheEnvironmentVariable(forMachine: false);
+            }*/
 
             builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
+
+            builder.Services.AddSwagger();
+            builder.Services.AddPostgreSQL(builder.Configuration);
+            builder.Services.AddRepositories();
+            builder.Services.AddServices();
+            builder.Services.AddJwtService(builder.Configuration);
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.MapOpenApi();
+                app.UseSwaggerInterface();
             }
 
             app.UseHttpsRedirection();
-
+            app.UseAuthentication();
             app.UseAuthorization();
-
-
             app.MapControllers();
 
             app.Run();
