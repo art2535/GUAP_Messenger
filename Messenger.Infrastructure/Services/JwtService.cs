99b5a519cb84019e9fe7c390c494f9389ed6a399
art2535 <artem010606@yandex.ru>
2025-10-12 19:21:33 +0300

    Созданы сервисы валидации данных и создания JWT

---
+++ Messenger.Infrastructure/Services/JwtService.cs
@@ -0,0 +1,53 @@
+﻿using Messenger.Core.Models;
+using Messenger.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Messenger.Infrastructure.Services
+{
+    public class JwtService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly GuapMessengerContext _context;
+
+        public JwtService(IConfiguration configuration, GuapMessengerContext context)
+        {
+            _configuration = configuration;
+            _context = context;
+        }
+
+        public async Task<string> GenerateJwtTokenAsync(User user, CancellationToken token = default)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.UserId.ToString()),
+                new Claim(ClaimTypes.Name, user.Login)
+            };
+
+            var userRoles = await _context.Users
+                .Where(u => u.UserId == user.UserId)
+                .SelectMany(u => u.Roles)
+                .ToListAsync(token);
+            foreach (var role in userRoles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role.Name));
+            }
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var jwtToken = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddHours(2),
+                signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(jwtToken);
+        }
+    }
+}
---
+++ Messenger.Infrastructure/Services/ValidationService.cs
@@ -0,0 +1,59 @@
+﻿using System.Text.RegularExpressions;
+using Bcrypt = BCrypt.Net.BCrypt;
+
+namespace Messenger.Infrastructure.Services
+{
+    public static class ValidationService
+    {
+        public static string HashPassword(string password)
+        {
+            return Bcrypt.HashPassword(password);
+        }
+
+        public static bool VerifyPassword(string password, string passwordHash)
+        {
+            return Bcrypt.Verify(password, passwordHash);
+        }
+
+        public static string ValidatePassword(string password)
+        {
+            if (string.IsNullOrWhiteSpace(password))
+                throw new Exception("Пароль не должен быть пустым");
+
+            if (!Regex.IsMatch(password, @".{8,20}"))
+                throw new Exception("Пароль должен содержать от 8 до 20 символов");
+
+            if (!Regex.IsMatch(password, @"[A-Z]"))
+                throw new Exception("Пароль должен содержать хотя бы одну заглавную букву (A-Z)");
+
+            if (!Regex.IsMatch(password, @"[a-z]"))
+                throw new Exception("Пароль должен содержать хотя бы одну строчную букву (a-z)");
+
+            if (!Regex.IsMatch(password, @"\d"))
+                throw new Exception("Пароль должен содержать хотя бы одну цифру (0-9)");
+
+            if (!Regex.IsMatch(password, @"[^\da-zA-Z]"))
+                throw new Exception("Пароль должен содержать хотя бы один специальный символ (например: !@#$%^&*)");
+
+            return password;
+        }
+
+        public static DateOnly ValidateBirthdate(DateTime dateTime)
+        {
+            if (dateTime > DateTime.UtcNow)
+            {
+                throw new Exception($"Дата рождения не может быть позднее {DateTime.UtcNow}");
+            }
+            return DateOnly.FromDateTime(dateTime);
+        }
+
+        public static string ValidateEmail(string email)
+        {
+            if (!Regex.IsMatch(email, @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"))
+            {
+                throw new FormatException("Email не соответствует формату");
+            }
+            return email;
+        }
+    }
+}
