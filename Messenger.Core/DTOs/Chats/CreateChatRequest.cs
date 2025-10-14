cbaa83c037bf416cfa287eef406a89deef57f15d
art2535 <artem010606@yandex.ru>
2025-10-14 14:48:51 +0300

    Созданы DTOs для чатов

---
+++ Messenger.Core/DTOs/Chats/CreateChatRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Messenger.Core.DTOs.Chats
+{
+    public class CreateChatRequest
+    {
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        public string Type { get; set; } = string.Empty;
+    }
+}
