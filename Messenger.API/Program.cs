db5b102e308046581c466506d43f2f5d44bcbcfb
art2535 <artem010606@yandex.ru>
2025-10-17 21:59:53 +0300

    Добавлена поддержка файла секретов

--- Messenger.API/Program.cs
+++ Messenger.API/Program.cs
@@ -8,10 +8,17 @@ namespace Messenger.API
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            /*if (builder.Environment.IsDevelopment())
+            if (!builder.Environment.IsDevelopment())
             {
                 JwtExtension.SetTheEnvironmentVariable(forMachine: false);
-            }*/
+                PostgreSQLExtension.SetTheEnvironmentVariable(forMachine: false);
+            }
+
+            builder.Configuration
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true)
+                .AddUserSecrets<Program>(optional: true)
+                .AddEnvironmentVariables();
 
             builder.Services.AddControllers();
 
