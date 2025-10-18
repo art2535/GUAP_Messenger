be860b6888dfe73a0f5ac1c232c8374f6b9df927
art2535 <artem010606@yandex.ru>
2025-10-18 17:13:59 +0300

    Обновлена сигнатура интерфейса сервиса статуса пользователей

--- Messenger.Core/Interfaces/IUserStatusService.cs
+++ Messenger.Core/Interfaces/IUserStatusService.cs
@@ -5,6 +5,6 @@ namespace Messenger.Core.Interfaces
     public interface IUserStatusService
     {
         Task UpdateStatusAsync(UserStatus userStatus, CancellationToken cancellationToken = default);
-        Task<UserStatus> GetStatusByUserIdAsync(Guid userId, CancellationToken cancellationToken = default);
+        Task<UserStatus?> GetStatusByUserIdAsync(Guid userId, CancellationToken cancellationToken = default);
     }
 }
