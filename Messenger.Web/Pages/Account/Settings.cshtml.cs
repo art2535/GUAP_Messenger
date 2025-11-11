c5b4f7ddfc533e16e9d3ec937b48a2eed40f2f69
art2535 <artem010606@yandex.ru>
2025-11-11 21:52:44 +0300

    Создана пустая страница с настройками

---
+++ Messenger.Web/Pages/Account/Settings.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Messenger.Web.Pages.Account
+{
+    public class SettingsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
