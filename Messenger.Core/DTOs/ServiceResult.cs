22897f076ec7049dc1cb1f44bdabe43e3924f967
art2535 <artem010606@yandex.ru>
2025-11-12 20:26:59 +0300

    Добавлен сервис для логирования

---
+++ Messenger.Core/DTOs/ServiceResult.cs
@@ -0,0 +1,22 @@
+﻿namespace Messenger.Core.DTOs
+{
+    public class ServiceResult<T>
+    {
+        public bool isSuccess { get; set; }
+        public T? data { get; set; }
+        public string? error { get; set; }
+        public string? innerError { get; set; }
+
+        public static ServiceResult<T> Success(T data) => new() 
+        { 
+            isSuccess = true, 
+            data = data 
+        };
+        public static ServiceResult<T> Failure(string error, string? innerError = null) => new() 
+        { 
+            isSuccess = false, 
+            error = error,
+            innerError = innerError
+        };
+    }
+}
