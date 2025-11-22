e9d6a5ec0c01a2d316c9f7c90d05402acef2e5cc
art2535 <artem010606@yandex.ru>
2025-11-22 20:34:35 +0300

    Создан DTO для динамического поиска пользователей, зарегистрированных в системе

---
+++ Messenger.Core/DTOs/Users/UserSearch.cs
@@ -0,0 +1,9 @@
+﻿namespace Messenger.Core.DTOs.Users
+{
+    public class UserSearch
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Avatar { get; set; }
+    }
+}
