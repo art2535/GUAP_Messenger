dd0c4b37697d7a1fb85b464c263779a851ade507
art2535 <artem010606@yandex.ru>
2026-03-13 22:35:07 +0300

    Добавлены DTOs для настроек аккаунта

---
+++ Messenger.Core/DTOs/Users/BlockedUserDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Messenger.Core.DTOs.Users
+{
+    public class BlockedUserDto
+    {
+        public string? Id { get; set; }
+        public string? Name { get; set; }
+        public string? Login { get; set; }
+        public string? Avatar { get; set; }
+    }
+}
---
+++ Messenger.Core/DTOs/Users/UserProfileDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Messenger.Core.DTOs.Users
+{
+    public class UserProfileDto
+    {
+        public string? UserId { get; set; }
+        public string? LastName { get; set; }
+        public string? FirstName { get; set; }
+        public string? MiddleName { get; set; }
+        public string? Login { get; set; }
+        public string? Phone { get; set; }
+        public AccountDto? Account { get; set; }
+    }
+}
