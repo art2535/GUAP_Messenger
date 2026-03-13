868e41d29d6833da8ab7fb07b29b2b7e8d0d17a1
art2535 <artem010606@yandex.ru>
2026-03-13 21:32:31 +0300

    Создан сервис для валидации пользователя по токену

---
+++ Messenger.API/Services/UserValidationService.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Messenger.Core.Interfaces;
+using Messenger.Core.Models;
+
+namespace Messenger.API.Services
+{
+    public static class UserValidationService
+    {
+        public static async Task<(User? User, IActionResult? Error)> GetCurrentUserOrErrorAsync(
+            ClaimsPrincipal user, IUserService userService)
+        {
+            var externalId = user.FindFirst("sub")?.Value
+                          ?? user.FindFirst("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier")?.Value;
+
+            if (string.IsNullOrEmpty(externalId))
+            {
+                return (null, new UnauthorizedObjectResult("Не найден внешний идентификатор (sub / nameidentifier)"));
+            }
+
+            var dbUser = await userService.GetUserByExternalIdAsync(externalId);
+            if (dbUser == null)
+            {
+                return (null, new BadRequestObjectResult("Пользователь с указанным внешним идентификатором не найден в системе"));
+            }
+
+            return (dbUser, null);
+        }
+
+        public static async Task<User> GetCurrentUserAsync(ClaimsPrincipal user, IUserService userService)
+        {
+            var (dbUser, error) = await GetCurrentUserOrErrorAsync(user, userService);
+
+            if (error != null)
+            {
+                throw new InvalidOperationException("Не удалось получить текущего пользователя: " +
+                    (error as ObjectResult)?.Value?.ToString() ?? "Неизвестная ошибка");
+            }
+
+            return dbUser!;
+        }
+    }
+}
