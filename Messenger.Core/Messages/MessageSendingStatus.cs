c4d114d8f75c9f45e55287d3fa00364ec1cb90e9
art2535 <artem010606@yandex.ru>
2026-03-30 18:14:19 +0300

    Добавлено класс сообщения при отправке сообщения для RabbitMQ

---
+++ Messenger.Core/Messages/MessageSendingStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace Messenger.Core.Messages
+{
+    public record MessageSendingStatus
+    {
+        public Guid MessageId { get; init; }
+        public Guid ChatId { get; init; }
+        public string Status { get; init; }
+        public string? Reason { get; init; }
+        public DateTimeOffset Timestamp { get; init; }
+    }
+}
