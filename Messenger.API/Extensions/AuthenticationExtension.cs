3d02c357c2de12091a3d13d39d333dd72a4e0abb
Артём Петров <artem010606@yandex.ru>
2026-08-23 21:00:05 +0300

    Переписано расширение для .NET 10

--- Messenger.API/Extensions/AuthenticationExtension.cs
+++ Messenger.API/Extensions/AuthenticationExtension.cs
@@ -7,81 +7,84 @@ namespace Messenger.API.Extensions
 {
     public static class AuthenticationExtension
     {
-        public static void AddEtaApiAuthentication(this IServiceCollection services, bool requireHttpsMetadata)
+        extension(IServiceCollection services)
         {
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddJwtBearer(options =>
+            public void AddEtaApiAuthentication(bool requireHttpsMetadata)
+            {
+                services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                    .AddJwtBearer(options =>
+                    {
+                        options.Authority = "https://sso.guap.ru/realms/master";
+                        options.RequireHttpsMetadata = requireHttpsMetadata;
+
+                        options.TokenValidationParameters = new TokenValidationParameters
+                        {
+                            ValidateIssuer = true,
+                            ValidIssuer = "https://sso.guap.ru/realms/master",
+
+                            ValidateAudience = true,
+                            ValidAudiences = ["messager", "account"],
+
+                            ValidateLifetime = true,
+                            ClockSkew = TimeSpan.FromMinutes(10),
+
+                            NameClaimType = "sub",
+                            RoleClaimType = "role",
+
+                            ValidateIssuerSigningKey = true
+                        };
+
+                        options.MapInboundClaims = false;
+                    });
+            }
+
+            public void AddEtaWebAuthentication(IConfiguration configuration)
+            {
+                services.AddAuthentication(options =>
+                {
+                    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                    options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+                    options.DefaultSignOutScheme = OpenIdConnectDefaults.AuthenticationScheme;
+                    options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                })
+                .AddCookie(options =>
+                {
+                    options.LoginPath = "/Authorization";
+                    options.ExpireTimeSpan = TimeSpan.FromHours(12);
+                    options.SlidingExpiration = true;
+
+                    options.Cookie.Name = ".GuapMessenger.Cookie";
+                    options.Cookie.SameSite = SameSiteMode.Lax;
+                    options.Cookie.HttpOnly = true;
+                    options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+                    options.Cookie.IsEssential = true;
+
+                    options.Cookie.Path = "/";
+                    options.Cookie.MaxAge = TimeSpan.FromHours(12);
+                })
+                .AddOpenIdConnect(options =>
                 {
                     options.Authority = "https://sso.guap.ru/realms/master";
-                    options.RequireHttpsMetadata = requireHttpsMetadata;
+                    options.ClientId = configuration["AzureAd:ClientId"];
+                    options.ClientSecret = configuration["AzureAd:ClientSecret"];
+                    options.CallbackPath = configuration["AzureAd:CallbackPath"];
+                    options.SignedOutCallbackPath = configuration["AzureAd:SignedOutCallbackPath"];
+                    options.ResponseType = "code";
+                    options.SaveTokens = true;
+                    options.GetClaimsFromUserInfoEndpoint = true;
 
-                    options.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        ValidateIssuer = true,
-                        ValidIssuer = "https://sso.guap.ru/realms/master",
+                    options.Scope.Add("openid");
+                    options.Scope.Add("profile");
+                    options.Scope.Add("email");
 
-                        ValidateAudience = true,
-                        ValidAudiences = ["messager", "account"],
+                    options.TokenValidationParameters.ValidateIssuer = true;
+                    options.TokenValidationParameters.NameClaimType = "name";
+                    options.TokenValidationParameters.RoleClaimType = "role";
 
-                        ValidateLifetime = true,
-                        ClockSkew = TimeSpan.FromMinutes(10),
-
-                        NameClaimType = "sub",
-                        RoleClaimType = "role",
-
-                        ValidateIssuerSigningKey = true
-                    };
-
-                    options.MapInboundClaims = false;
+                    options.UseTokenLifetime = true;
                 });
-        }
-
-        public static void AddEtaWebAuthentication(this IServiceCollection services, IConfiguration configuration)
-        {
-            services.AddAuthentication(options =>
-            {
-                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
-                options.DefaultSignOutScheme = OpenIdConnectDefaults.AuthenticationScheme;
-                options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-            })
-            .AddCookie(options =>
-            {
-                options.LoginPath = "/Authorization";
-                options.ExpireTimeSpan = TimeSpan.FromHours(12);
-                options.SlidingExpiration = true;
-
-                options.Cookie.Name = ".GuapMessenger.Cookie";
-                options.Cookie.SameSite = SameSiteMode.Lax; 
-                options.Cookie.HttpOnly = true;
-                options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
-                options.Cookie.IsEssential = true;
-
-                options.Cookie.Path = "/";
-                options.Cookie.MaxAge = TimeSpan.FromHours(12);
-            })
-            .AddOpenIdConnect(options =>
-            {
-                options.Authority = "https://sso.guap.ru/realms/master";
-                options.ClientId = configuration["AzureAd:ClientId"];
-                options.ClientSecret = configuration["AzureAd:ClientSecret"];
-                options.CallbackPath = configuration["AzureAd:CallbackPath"];
-                options.SignedOutCallbackPath = configuration["AzureAd:SignedOutCallbackPath"];
-                options.ResponseType = "code";
-                options.SaveTokens = true;
-                options.GetClaimsFromUserInfoEndpoint = true;
-
-                options.Scope.Add("openid");
-                options.Scope.Add("profile");
-                options.Scope.Add("email");
-
-                options.TokenValidationParameters.ValidateIssuer = true;
-                options.TokenValidationParameters.NameClaimType = "name";
-                options.TokenValidationParameters.RoleClaimType = "role";
-
-                options.UseTokenLifetime = true;
-            });
+            }
         }
     }
 }
