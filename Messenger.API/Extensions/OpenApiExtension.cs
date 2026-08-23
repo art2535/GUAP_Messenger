50e5dd1c265e0c772d2d7a772441ab6e82c55f54
Артём Петров <artem010606@yandex.ru>
2026-08-23 21:03:04 +0300

    Полностью убран Swagger UI из расширения

--- Messenger.API/Extensions/OpenApiExtension.cs
+++ Messenger.API/Extensions/OpenApiExtension.cs
@@ -48,30 +48,5 @@ namespace Messenger.API.Extensions
                 });
             }
         }
-
-        public static void AddSwagger(this IServiceCollection services)
-        {
-            services.AddEndpointsApiExplorer();
-            services.AddSwaggerGen(options =>
-            {
-                options.SwaggerDoc("v1", new OpenApiInfo
-                {
-                    Version = "v1",
-                    Title = "GUAP Messenger API",
-                    Description = "API для управления мессенджером ГУАП"
-                });
-                options.EnableAnnotations();
-            });
-        }
-
-        public static void UseSwaggerInterface(this IApplicationBuilder builder)
-        {
-            builder.UseSwagger();
-            builder.UseSwaggerUI(options =>
-            {
-                options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-                options.RoutePrefix = string.Empty;
-            });
-        }
     }
 }
